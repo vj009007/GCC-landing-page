f513ddca1daa5dfa8b77c36d8f1a4205eabd04d0
mohdrizwan <raza25409@gmail.com>
2023-11-01 15:50:57 +0530

    upload files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#a79a66',
+        secondary: '#191919',
+      },
+    },
+  },
+  plugins: [],
+}
